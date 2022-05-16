d09ca7915c52234e2e4060638ad56527e1ed8af6
matcinks <x.mateuszstepien@gmail.com>
2022-05-16 22:44:14 +0200

    Initial commit.

---
+++ src/main/java/pl/ilpiu/clothingfactory/ClothingFactoryApplication.java
@@ -0,0 +1,13 @@
+package pl.ilpiu.clothingfactory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClothingFactoryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClothingFactoryApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/ilpiu/clothingfactory/ClothingFactoryApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.ilpiu.clothingfactory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClothingFactoryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
